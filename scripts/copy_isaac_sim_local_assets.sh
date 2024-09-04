9f4945928751d3854108d5ed1805cd0904ee6331
Quillon Sun <66347106+pdl0x0lbq@users.noreply.github.com>
2024-09-04 17:20:07 +0800

    Update copy_isaac_sim_local_assets.sh

--- scripts/copy_isaac_sim_local_assets.sh
+++ scripts/copy_isaac_sim_local_assets.sh
@@ -10,18 +10,18 @@ mkdir -p ~/Downloads/isaac_sim_assets_final_package
 # Copy package 1 to final_package
 echo "Copy package 1 to final_package"
 sleep 3
-cp -v -r ~/Downloads/isaac-sim-assets-1-4.0.0/* ~/Downloads/isaac_sim_assets_final_package/
+cp -v -r ~/Downloads/isaac-sim-assets-1-4.1.0/* ~/Downloads/isaac_sim_assets_final_package/
 # Copy package 2 to final_package
 echo "Copy package 2 to final_package"
 sleep 3
-cp -v -r ~/Downloads/isaac-sim-assets-2-4.0.0/* ~/Downloads/isaac_sim_assets_final_package/
+cp -v -r ~/Downloads/isaac-sim-assets-2-4.1.0/* ~/Downloads/isaac_sim_assets_final_package/
 # Copy package 3 to final_package
 echo "Copy package 3 to final_package"
 sleep 3
-cp -v -r ~/Downloads/isaac-sim-assets-3-4.0.0/* ~/Downloads/isaac_sim_assets_final_package/
+cp -v -r ~/Downloads/isaac-sim-assets-3-4.1.0/* ~/Downloads/isaac_sim_assets_final_package/
 # Copy package 4 to final_package
 echo "Copy package 4 to final_package"
 sleep 3
-cp -v -r ~/Downloads/isaac-sim-assets-4-4.0.0/* ~/Downloads/isaac_sim_assets_final_package/
+cp -v -r ~/Downloads/isaac-sim-assets-4-4.1.0/* ~/Downloads/isaac_sim_assets_final_package/
 
 echo "All assets are copied to final_package"
